cf8146b8d254c56807b612de157ec8a96d08d6be
koteitan <ukaibutton@mac.com>
2018-03-16 02:21:48 +0900

    v2.2 is added.
    v2.2:
    - The concestor of multi rows become the bad part.
    - Delta is added into all elements.

--- basmat.c
+++ basmat.c
@@ -28,20 +28,21 @@ Example:
      basmat -o 4 -s 100000 "(0,0)(1,1)[3]"
      basmat -o 1 -s 13000 -t 100000 "(0,0)(1,1)[3]"
 
-The original version 2.0 is available at
-  http://gyafun.jp/ln/basmat/
-
 Version History:
-     Version 2.1 (MAR    2018): by koteitan
-     Version 2.0 (APR 14 2017): by Fish
-     Version 1.0 (SEP  1 2014): by Fish
+     Version 1.0 (SEP  1 2014): Algorithm by Bashicu,  Program by Fish
+     Version 2.0 (APR 14 2017): Algorithm by Bashicu,  Program by Fish
+     Version 2.1 (MAR    2018): Algorithm by koteitan, Program by koteitan
+     Version 2.2 (MAR    2018): Algorithm by koteitan, Program by koteitan
+     http://ja.googology.wikia.com/wiki/ユーザーブログ:BashicuHyudora/BASIC言語による巨大数のまとめ#.E3.83.90.E3.82.B7.E3.82.AF.E8.A1.8C.E5.88.97.E6.95.B0.28Bashicu_matrix_number.29
      https://github.com/koteitan/basmat-2.1
      http://gyafun.jp/ln/basmat/ChangeLog.txt
 
 Authors:
-  Original version 2.0: Fish
+  Bashicu
+    http://ja.googology.wikia.com/wiki/ユーザーブログ:BashicuHyudora
+  Fish
     http://googology.wikia.com/wiki/User:Kyodaisuu
-  Modified version 2.1: koteitan
+  koteitan
     https://twitter.com/koteitan
 
 ***********************************/
@@ -51,10 +52,70 @@ Authors:
 #include <limits.h>
 #include <string.h>
 #include <unistd.h>
- 
+
+void printM(int *A, int nr, int nc){
+  int r,c;
+  for(c=0;c<nc;c++){
+    printf("(");
+    for(r=0;r<nr-1;r++)printf("%d,",A[r+c*nr]);
+    if(nr>0)printf("%d)",A[nr-1+c*nr]);
+  }
+}
+void printA(int *A, int nr){
+  int r=0;
+  printf("(");
+  for(r=0;r<nr-1;r++)printf("%d,",A[r]);
+  if(nr>0)printf("%d)",A[nr-1]);
+}
+/* returns index of the element x in P with nr rows (it returns -1 when x is not found) */
+int find(int *P, int nr, int x){
+  int i;
+  for(i=0;i<nr;i++) if(P[i]==x) return i;
+  return -1;
+}
+/* are the all elements equal in nr rows of P */
+int areAllEq(int *P, int nr){
+  int i, p0=P[0];
+  for(i=1;i<nr;i++) if(P[i]!=p0) return 0;
+  return -1;  
+}
+/* argmin returns the index whose element is minimum of P in nr rows */
+int argmax(int *P, int nr){
+  int i, m=nr;
+  for(i=0;i<nr;i++) if(P[i]>P[m]) m=i;
+  return m;
+}
+/* getParent returns the column number of the parent of the child column c in row r in Bashicu matrix S consist of the nr rows (it returns -1 when parent is not found) */
+int getParent(int *S, int r, int c, int nr){
+  int i;
+  for(i=c;i>=0;i--) if(S[r+i*nr]<S[r+c*nr]) return i;
+  return -1; 
+}
+/* getConcestor returns the column number of the concestor of with m rows from the child column c in Bashicu matrix S consist of the nr rows (it returns -1 when concestor is not found) */
+int getConcestor(int *S, int m, int c, int nr){
+  int r, maxr;
+  int *P = malloc(sizeof(int) * nr);
+  
+  for(r=0;r<m;r++) P[r] = getParent(S,r,c,nr);
+  while(find(P,m,-1)==-1){
+    if(areAllEq(P,m)){
+      /* same */
+      r = P[0];
+      free(P);
+      return r;
+    }else{
+      /* not same */
+      maxr = argmax(P,m);
+      P[maxr]=getParent(S,maxr,P[maxr],nr);
+    }
+  }
+  free(P);
+  return -1;
+}
+
 int main(int argc, char *argv[]) {
   /**************************** Initialization ****************************/
-  int *S, *Delta, *C, row=0, bad, found, i=0, j, l, m, nr, len;
+  int *S, *Delta, *C, *P, row=0, bad, found, i=0, j, l, m, nr, len;
   int opt=1, ver=210, detail=0, help=0;
   long n, nn, k, num=0, s=20, step=0, maxstep=0;
   char *bm, arg;
@@ -66,6 +127,7 @@ int main(int argc, char *argv[]) {
           if     (strcmp(optarg,"1.0")==0||strcmp(optarg,"1")==0)ver=100;
           else if(strcmp(optarg,"2.0")==0||strcmp(optarg,"2")==0)ver=200;
           else if(strcmp(optarg,"2.1")==0                       )ver=210;
+          else if(strcmp(optarg,"2.2")==0                       )ver=220;
           else{
             printf("Error: invalid version.\n");
             help=1;
@@ -109,9 +171,9 @@ int main(int argc, char *argv[]) {
   nr = row+1;
 
   /* Allocate array */
-  S = malloc(sizeof(int) * nr * s);
+  S     = malloc(sizeof(int) * nr * s);
   Delta = malloc(sizeof(int) * nr);
-  C = malloc(sizeof(int) * nr * s);
+  C     = malloc(sizeof(int) * nr * s);
   if (S == NULL || Delta == NULL || C == NULL) {
     printf("Allocation failed.\n");
     exit(-1);
@@ -257,7 +319,7 @@ int main(int argc, char *argv[]) {
             }
             printf ("\n");
         }
-      } else if(ver == 210){
+      }else if(ver == 210){
         /***** Version 2.1 *****/
         /* Calculate m = Uppermost zero at the rightmost sequence */
         m = row+1; /* row = number of rows + 1 */
@@ -313,6 +375,25 @@ int main(int argc, char *argv[]) {
             }
           }else{k=k-1;}
         }
+      }else if(ver == 220){
+        /***** Version 2.2 *****/
+        /* Calculate m = Uppermost zero at the rightmost sequence */
+        m = row+1; /* row = number of rows + 1 */
+        for(j=0; j<=row; j++){
+          if (S[j + n*nr] == 0) m=j, j=row+1;
+        }
+        /* Determine the bad sequence and calculate Delta */
+        l=getConcestor(S,m,n,nr);
+        if(l>=0){
+          bad=n-l;
+        }else{
+          /* no concestor */
+          bad=0;
+        }
+        /* If all dimension are small, calculate Delta */
+        for(j=0; j<=row; j++){
+          Delta[j] = (j<m-1) ? S[j+n*nr]-S[j+(n-bad)*nr]:0;
+        }
       }
     }
 
@@ -359,7 +440,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* Copy bad sequence */
-    if (ver == 100 || ver == 210) {
+    if (ver == 100 || ver == 210 || ver == 220) {
     /***** Version 1 *****/
       while (n < nn) {
         for (l=0; l<=row; l++) S[l + n*nr] = S[l + (n-bad)*nr] + Delta[l];
